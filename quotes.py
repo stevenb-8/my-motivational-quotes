964663ebc96ea309782f0bcd7b304fd6b9c11fcb
Steven Boyd <stevenboyd@Stevens-MacBook-Pro-3.local>
2025-04-28 16:41:39 -0700

    Add first version of random quote generator

---
+++ quotes.py
@@ -0,0 +1,9 @@
+import random
+
+quotes = [
+    "Believe you can and you're halfway there.",
+    "You are stronger than you think.",
+    "Success is the sum of small efforts repeated day in and day out."
+]
+
+print(random.choice(quotes))
