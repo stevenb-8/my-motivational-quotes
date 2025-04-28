f00e439c2e034b249fc4ce5531cc5bd095f46ef5
Steven Boyd <stevenboyd@Stevens-MacBook-Pro-3.local>
2025-04-28 16:52:52 -0700

    Added a new quote about beaches

--- quotes.py
+++ quotes.py
@@ -4,6 +4,7 @@ quotes = [
     "Believe you can and you're halfway there.",
     "You are stronger than you think.",
     "Success is the sum of small efforts repeated day in and day out."
+    "Life is a beach and you are a big fat whale."
 ]
 
 print(random.choice(quotes))
